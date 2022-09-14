452d826c937d155dca0e274b71a480d5c15b95a5
Dio <dioginisaugusto1@gmail.com>
2022-09-13 21:39:57 -0300

    mobile

--- mobileVer/src/screens/Home/index.tsx
+++ mobileVer/src/screens/Home/index.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import { View, Image } from 'react-native';
+
+import { View, Image, FlatList } from 'react-native';
 import logo from '../../assets/logo-nlw-esports.png'
+import { GamingCard } from '../../components/GamingCard';
 import { Heading } from '../../components/Heading';
 import { styles } from './styles';
+import { GAMES } from '../../utils/games'
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Image 
+      <Image
         source={logo}
         style={styles.logo}
       />
 
-      <Heading 
+      <Heading
         title="Encontre seu duo"
         subtitle="Selecione o game que deseja jogar"
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <GamingCard
+            data={item}
+          />)}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
       />
     </View>
   );
