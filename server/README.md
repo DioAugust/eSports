d158e3069d712384aa9b39f5df0e80225987d5a7
Dio <dioginisaugusto1@gmail.com>
2022-09-14 22:39:45 -0300

    configurando back, adicionando prisma

---
+++ server/README.md
@@ -0,0 +1,26 @@
+# Back-end
+
+## Entidades 
+
+### Game
+    id
+    title
+    bannerUrl
+
+### AD
+    id
+    gameId
+    name
+    yearsPlaying
+    discord
+    weekDays
+    hourStart
+    hourEnd
+    useVoiceChannel
+    createdAt
+
+## Casos de uso
+    - Listagem de games com contagem de anuncios
+    - Criacao de novo anuncios
+    - Listagem de anuncios por game
+    - Buscar discord pelo id do anuncio
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,18 @@
 import express  from "express";
+const app = express();
 
-const app = express(
+app.get('/games', (request, response) => {
+    return response.json([]);
+})
 
-);
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+})
 
-app.get('/ads',  (request, response) => {
+
+app.get('/games/:id/ads',  (request, response) => {
+    // const gamesId = request.params.id;
+
     return response.json([
         {id: 1, name: 'anuncio1'},
         {id: 2, name: 'anuncio2'},
@@ -12,6 +20,12 @@ app.get('/ads',  (request, response) => {
         {id: 4, name: 'anuncio4'},
         {id: 5, name: 'anuncio5'},
     ])
+});
+
+app.get('ads/:id/discord',  (request, response) => {
+    // const adId = request.params.id;
+
+    return response.json([])
 });
 
 app.listen(3333)
