ff919ef29a6ac76b37e8f26d1bf5924760e1380e
Dio <dioginisaugusto1@gmail.com>
2022-10-10 09:44:53 -0300

    resolvido erro do modal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -61,12 +61,10 @@ function App() {
 
           
             <form className="mt-8 flex flex-col gap-4">
+
               <div className="flex flex-col gap-2">
                 <label className="font-semibold" htmlFor="game">Qual o game?</label>
-                <Input 
-                  id="game" 
-                  placeholder="Selecione o game que deseja jogar"
-                />
+                <Input id="game" placeholder="Selecione o game que deseja jogar"/>
               </div>
 
               <div className="flex flex-col gap-2">
@@ -75,7 +73,6 @@ function App() {
               </div>
 
               <div className="grid grid-cols-2 gap-6">
-
                 <div className="flex flex-col gap-2">
                   <label htmlFor="yearsPlaying">Joga a quantos anos?</label>
                   <Input id="yearsPlaying" type="number" placeholder="Tudo bem ser ZERO"/>
@@ -83,28 +80,30 @@ function App() {
 
                 <div className="flex flex-col gap-2">
                   <label htmlFor="discord">Qual seu discord?</label>
-                  <Input id="discord" placeholder="Usuario#0000" />
+                  <Input id="discord" type="text" placeholder="Usuario#0000" />
                 </div>
-                
-                <div className="flex gap-6">
+
+              </div>
+
+              <div className="flex gap-6">
                   <div className="flex flex-col gap-2">
                     <label htmlFor="weekDays">Quando costuma jogar?</label>
 
-                    <div>
+                    <div className="grid grid-cols-4 gap-2">
                       <button 
-                        className="w-8 h-8" title="Domingo"></button>
+                        className="w-8 h-8 rounded bg-zinc-900" title="Domingo">D</button>
                       <button 
-                        className="w-8 h-8" title="Segunda"></button>
+                        className="w-8 h-8 rounded bg-zinc-900" title="Segunda">S</button>
                       <button 
-                        className="w-8 h-8" title="Terça"></button>
+                        className="w-8 h-8 rounded bg-zinc-900" title="Terça">T</button>
                       <button 
-                        className="w-8 h-8" title="Quarta"></button>
+                        className="w-8 h-8 rounded bg-zinc-900" title="Quarta">Q</button>
                       <button 
-                        className="w-8 h-8" title="Quinta"></button>
+                        className="w-8 h-8 rounded bg-zinc-900" title="Quinta">Q</button>
                       <button 
-                        className="w-8 h-8" title="Sexta"></button>
+                        className="w-8 h-8 rounded bg-zinc-900" title="Sexta">S</button>
                       <button 
-                        className="w-8 h-8" title="Sábado"></button>
+                        className="w-8 h-8 rounded bg-zinc-900" title="Sábado">S</button>
                     </div>
                   </div>
 
@@ -115,18 +114,20 @@ function App() {
                       <Input type="time" id="hourEnd" placeholder="Ate"/>
                     </div>
                   </div>
-
                 </div>
-              </div>
 
-              <div>
+              <div className="mt-2 flex gap-2 text-sm">
                   <Input type="checkbox"/>Costumo me conectar no chat de voz.
               </div>
 
-                <footer>
-                  <button>Cancelar</button>
-                  <button type="submit">
-                    <GameController/>
+                <footer className="mt-4 flex justify-end gap-4">
+                  <Dialog.Close 
+                  type="button"
+                  className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600">Cancelar</Dialog.Close>
+                  <button 
+                  type="submit" 
+                  className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600">
+                    <GameController size="24"/>
                     Encontrar duo
                   
                   </button>
